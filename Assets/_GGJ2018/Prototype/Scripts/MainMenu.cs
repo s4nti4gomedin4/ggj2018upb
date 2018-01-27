46b9f169e872aa0ecd632d16857c2ae286563a9f
DARKCORTEX <dark_cortex2@hotmail.com>
2018-01-27 18:58:19 -0500

    Intros

---
+++ Assets/_GGJ2018/Prototype/Scripts/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public GameObject[] intros;
+	public enum IntroSlides
+	{
+		mainMenu,
+		intro1,
+		intro2,
+		intro3
+	};
+
+	public IntroSlides actualSlide;
+
+	public static MainMenu instance;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		intros [0].SetActive (actualSlide == IntroSlides.mainMenu ? true: false);
+		intros [1].SetActive (actualSlide == IntroSlides.intro1 ? true: false);
+		intros [2].SetActive (actualSlide == IntroSlides.intro2 ? true: false);
+		intros [3].SetActive (actualSlide == IntroSlides.intro3 ? true: false);
+
+		if (Input.anyKeyDown || Input.GetMouseButtonDown (0)) {
+			if (actualSlide == IntroSlides.intro1) {
+				actualSlide = IntroSlides.intro2;
+			} else if (actualSlide == IntroSlides.intro2) {
+				actualSlide = IntroSlides.intro3;
+			}else if(actualSlide == IntroSlides.intro3)
+			{
+				SceneManager.LoadScene ("Main");
+			}
+		}
+	}
+
+	public void StartBtn()
+	{
+		actualSlide = IntroSlides.intro1;
+	}
+		
+}
---
+++ Assets/_GGJ2018/Prototype/Scripts/NextIntro.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextIntro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Next()
+	{
+		if (MainMenu.instance.actualSlide == MainMenu.IntroSlides.intro1) {
+			MainMenu.instance.actualSlide = MainMenu.IntroSlides.intro2;
+		} else if (MainMenu.instance.actualSlide == MainMenu.IntroSlides.intro2) {
+			MainMenu.instance.actualSlide = MainMenu.IntroSlides.intro3;
+		} else if (MainMenu.instance.actualSlide == MainMenu.IntroSlides.intro3) {
+			SceneManager.LoadScene ("Main");
+		}
+	}
+}
