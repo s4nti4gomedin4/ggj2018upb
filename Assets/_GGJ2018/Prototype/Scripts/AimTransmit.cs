4803e31b365bf3f788098dedb61a710a5d9fe37b
Michael Alexander Sierra <csierrma@onjci.com>
2018-01-27 18:57:14 -0500

    add cursor movement to aim zone

--- Assets/_GGJ2018/Prototype/Scripts/AimTransmit.cs
+++ Assets/_GGJ2018/Prototype/Scripts/AimTransmit.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AimTransmit : MonoBehaviour {
+	public float m_ReticleSpeed=0.01f;
+
 	public GameObject m_AimZone;
 	public GameObject m_AimReticle;
+	private float lastPosX =  0;
+	private float lastPosY =  0;
 
 	private Vector3 mousePositionOnAim;
-	private const float maxRange=0.48f;
+	private const float maxRange=4f;
 	private const float minRange=-1*maxRange;
 	private const float initialZ=0.13f;
 
-	private float mouseOldPositionX=0;
-	private float mouseOldPositionY=0;
-
-	private bool isActive;
+	public bool isActive;
 	// Use this for initialization
 	void Start () {
 		DrawAimZone (isActive);
 	}
-	
+
+
+	void OnDisable (){
+		isActive = false;
+	}
 	// Update is called once per frame
 	void Update () {
 		var transmit=Input.GetButtonDown("Jump");
@@ -28,29 +33,27 @@ public class AimTransmit : MonoBehaviour {
 			DrawAimZone (isActive);
 		}
 		if (isActive) {
+			
+			var moveX = Input.GetAxis ("Move X");
+			var moveY = Input.GetAxis ("Move Y");
 
-			var moveX = Mathf.Clamp(Input.GetAxis ("Move X"),-1,1);
-			var moveY = Mathf.Clamp(Input.GetAxis ("Move Y"),-1,1);
-
-			var valueX = GetLerpedValue (moveX);
-			var valueZ = GetLerpedValue (moveY);
-
-			var currentPos = m_AimReticle.transform.localPosition;
-
-			currentPos.x = Mathf.Clamp(currentPos.x+valueX,minRange,maxRange) ;
-			currentPos.z = Mathf.Clamp(currentPos.z+valueZ,minRange,maxRange) ;
-
-			//m_AimReticle.transform.localPosition = currentPos;
-
-				
+			var deltaMouseX = moveX - lastPosX;
+			var deltaMouseY = moveY - lastPosY;
+			if (deltaMouseX != 0 || deltaMouseY!=0) {
+				m_AimReticle.transform.position = m_AimReticle.transform.position + new Vector3 (deltaMouseX,0,deltaMouseY);
+			}
+			var distance = m_AimReticle.transform.position - transform.position;
+			if (distance.magnitude > maxRange) {
+				distance=distance.normalized*maxRange;
+				m_AimReticle.transform.position = transform.position + distance;
+			}
 		}
-
 	}
-	private float GetLerpedValue(float value){
+		
+	private float GetMovementValue(float value){
 
 		var direction = Mathf.Sign(value);
-		var finalpos = Mathf.Lerp (minRange, maxRange, Mathf.Abs (value));
-		return finalpos*direction;
+		return m_ReticleSpeed*direction*Time.deltaTime;
 			
 	}
 	private void DrawAimZone(bool activate){
